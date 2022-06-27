f41bb026bb83fda8f06e7eb58124b731c205cbd6
harukasu4523 <airgir23@gmail.com>
2022-06-27 20:25:50 +0900

    cpu

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,197 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+///////////////my code//////////////////
+(INIT)
+  @FLAG
+  M=0   // initialize FLAG
+(MONITOR)  // KEYが押されると黒に描画押されていないときは白に描画
+  @50  // 画面の最大値(512 * 256 / 16) = 8192がはいる (テストで50を入れてる)
+  D=A
+  @i
+  M=D //i = 8192(test = 50)
+  @MAP // SCREEN offset
+  M=0
+  @KBD
+  MD=M
+  @DRAW
+  D;JGT  // if (Memory[KBD] > 0) goto DRAW
+  @ERASER
+  D;JEQ  // if(Memory[KBD] == 0) goto ERASER
+(DRAW)
+  @FLAG
+  M=1      // flag = 1
+  @i
+  MD=M-1   // i--
+  @MONITOR
+  D;JLT    // if (i < 0)goto MONITOR
+  @MAP
+  D=M
+  @SCREEN
+  A=A+D    //  Memory[SCREEN + MAP]
+  M=-1     // DRAW black
+  @MAP
+  M=M+1    // MAP++
+  @DRAW
+  0;JMP
+  (ERASER)
+  @FLAG   
+  D=M
+  @MONITOR
+  D;JEQ    // if(flag == 0) goto MONITOR
+  @i
+  MD=M-1  
+  @INIT   
+  D;JLT    // if(i < 0) goto INIT
+  @MAP
+  D=M
+  @SCREEN
+  A=A+D
+  M=0
+  @MAP
+  M=M+1
+  @ERASER
+  0;JMP
+
+
+
+/////////////////another code///////////////////
+// // define screen size
+//     @8192
+//     D=A
+//     @number_of_pixel
+//     M=D
+
+//     @R0
+//     M=0     // Screen is empty at the start
+// (LOOP)
+//     @KBD
+//     D=M
+//     @KEY_PUSHED
+//     D;JNE   // if KBD != 0 then goto KEY_PUSHED
+// (KEY_NOT_PUSHED)
+//     @R1
+//     M=0     // R0=0
+//     @CHECK_STATE_CHANGE
+//     0;JMP
+// (KEY_PUSHED)
+//     @R1
+//     M=1     // R1=1
+// (CHECK_STATE_CHANGE)    // if R0 != R1 then screen is update
+//     @R0
+//     D=M     // D=R0
+//     @R1
+//     D=D-M   // D=R0-R1
+//     @LOOP
+//     D;JEQ   // if D == 0 then goto LOOP
+//     @i
+//     M=0
+//     @R1
+//     D=M
+//     @R0
+//     M=D
+//     @EMPTY_SCREEN_LOOP
+//     D;JEQ
+// (FILL_SCREEN_LOOP)
+//     @i
+//     D=M
+//     @number_of_pixel
+//     D=M-D
+//     @LOOP
+//     D;JLT
+//     @SCREEN
+//     A=A+D
+//     M=-1
+//     @i
+//     MD=M+1
+//     @FILL_SCREEN_LOOP
+//     0;JMP
+// (EMPTY_SCREEN_LOOP)
+//     @i
+//     D=M
+//     @number_of_pixel
+//     D=M-D
+//     @LOOP
+//     D;JLT
+//     @SCREEN
+//     A=A+D
+//     M=0
+//     @i
+//     MD=M+1
+//     @EMPTY_SCREEN_LOOP
+//     0;JMP
+
+
+
+
+
+
+
+
+
+
+
+
+// keybordの入力調べ
+
+// (LOOP)
+//   @KBD  //入力があればmemory[KBD] = ASCIIになる
+//   MD=M
+//   @i  // i = memory[KBD]
+//   M=D
+//   @LOOP
+//   0;JMP
+
+
+
+// gotoを使うときはDを参照にしないとうまくいかない
+// Dは数値をいじるとかコンペアしたり参照するときに使う
+// Mは値の格納
+// Aはメモリの位置を操作する、定数の格納
+
+
+/////////////画面描画のアルゴリズム////////////////////
+
+//     @50 // 画面のはじっこ
+//     D=A
+//     @i
+//     M=D  // i=8192
+//     @MAP // screenからのオフセット
+//     M=0
+// (LOOP)
+//     @i
+//     MD=M-1  // i--
+//     // D=M
+//     @END
+//     D;JLT // if(i < 0) goto END
+//     @MAP
+//     D=M
+//     @SCREEN
+//     A=A+D  // Memory[SCREEN + MAP]
+//     M=-1  //　Memory[SCREEN + MAP] = -1
+//     @MAP
+//     M=M+1  //MAP++
+//     @LOOP  // goto LOOP
+//     0;JMP
+// (END)
+//     @END
+//     0;JMP
+
+////////////メモリ操作のテスト///////////////
+//     @dlaw   // memory[16] = 0
+//     M=0 
+//     @i
+//     M=1
+// (LOOP)
+//     @i
+//     D=M    // memory[17] = 1
+//     @dlaw
+//     A=A+D // memory[16 + i]
+//     MD=1  //  memory[16 + i] = 1
+//     @i
+//     M=M+1  // memory[17] ++
+
+//     @LOOP
+//     0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,60 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+//test
+    @50
+    M=A //Memory[50]に50が入る
+    
+
+
+
+//　ほんまもんのやつ
+//     @R2 //A命令
+//     M=0 // Memory[R2] = 0 → R2のアドレスに0を入れる
+//     @i
+//     M=1
+// (LOOP)
+//     @i //A命令
+//     D=M // DレジスタにMemory[i]を代入する
+//     @R1 
+//     D=D-M //D = i - R1 -> Dレジスタに(t-1)のDレジスタ-Memory[R1]を代入する
+//     @END // A命令?
+//     D;JGT //if(D > 0) goto END ->(t-1)Dレジスタの値が0以下の場合ENDに飛ぶ
+//     @R0
+//     D=M // D = R0
+//     @R2
+//     M=D+M // R2 = R2 + R0
+//     @i
+//     M=M+1 //i++
+//     @LOOP
+//     0;JMP
+// (END)
+//     @END
+//     0;JMP
+    
+
+
+
+//     @i
+//     M=1
+//     @sum
+//     M=0
+// (LOOP)
+//     @i
+//     D=M
+//     @100
+//     D=D-A
+//     @END
+//     D;JGT
+//     @i
+//     D=M
+//     @sum
+//     M=D+M
+//     @i
+//     M=M+1
+//     @LOOP
+//     0;JMP
+// (END)
+//     @END
+//     0;JMP
